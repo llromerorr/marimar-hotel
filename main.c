bd134d2f7dd45f6ae751dc6086f718818b75ed69
Luis Romero <creytuning@hotmail.com>
2017-09-24 13:53:46 -0400

    New Reservation Quantity Checking

--- main.c
+++ main.c
@@ -4,18 +4,12 @@
 #include "headers/Room.h"
 
 int main(void){
-    /*
     Application_Menu_Welcome();
 
     while(Application_Menu_Main());
     
     Reservation_Memory_Clear();
     Charge_Memory_Clear();
-    */
-    
-    int * p = Room_Search_Free_ByAllPointer();
-    for(int i = 0; p[i]; i++)
-        printf("%d ", p[i]);
-    Console_Pause();
+
     return EXIT_SUCCESS;
 }
